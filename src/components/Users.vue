036096c9751ac9d61e05a90becb8602418b07d91
Jason Lu <zhanluzhanlu@163.com>
2017-03-14 03:39:15 +0800

    Add Vue-Resource and Vue-Router

--- src/components/Users.vue
+++ src/components/Users.vue
@@ -1,6 +1,21 @@
 <template>
     <div class="users">
         <h1>Users</h1>
+        <form @submit="addUser">
+            <input type="text" v-model="newUser.name" placeholder="Enter Name">
+            <br>
+            <input type="text" v-model="newUser.email" placeholder="Enter Email">
+            <br>
+            <input type="submit" value="Submit">
+        </form>
+        <ul>
+            <li v-for="user in users">
+                <input type="checkbox" class="toggle" v-model="user.contacted">
+                <span :class="{contacted:user.contacted}">
+                   {{user.name}}:{{user.email}}<button @click="deleteUser(user)">X</button>
+                  </span>
+            </li>
+        </ul>
     </div>
 </template>
 
@@ -9,13 +24,37 @@
     export default{
         name: 'users',
         data(){
-            return {}
+            return {
+                newUser: {},
+                users: []
+            }
         },
-        methods: {},
+        methods: {
+            addUser: function (e) {
+                this.users.push({
+                    name: this.newUser.name,
+                    email: this.newUser.email,
+                    contacted: false
+                });
+                e.preventDefault();
+            },
+            deleteUser: function (user) {
+                this.users.splice(this.users.indexOf(user), 1);
+
+            }
+        },
+        created:function () {
+            this.$http.get('https://jsonplaceholder.typicode.com/users')
+                .then(function (response) {
+                this.users = response.data;
+                });
+        }
     }
 </script>
 
 <style scoped>
-
+    .contacted {
+        text-decoration: line-through;
+    }
 
 </style>
